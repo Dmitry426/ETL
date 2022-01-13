db1ed7b7a5015efacc31e5b3ac839b8508a3601c
Dmitry426 <kachkindmitry@gmail.com>
2022-01-13 20:33:12 +0300

    Code duplicates removed

--- data_merger.py
+++ data_merger.py
@@ -14,28 +14,27 @@ class Data_Merger:
             "writers": [],
             "actors": [],
         }
-        self.result_merged = self.desired_structure
     def  combine_tables(self,obj:dict):
-        self.result_merged ["fw_id"] = obj['film_id']
-        self.result_merged ['imdb_rating'] = obj['rating']
-        self.result_merged ['title'] = obj['title']
-        self.result_merged ["description"] = obj['description']
-        if obj["role"] == "director" and obj["full_name"] != self.result_merged ["director"]:
-            self.result_merged ['director'] = obj['full_name']
-        if obj["role"] == "actor" and obj["full_name"] not in self.result_merged ["actors_names"]:
-            self.result_merged ["actors_names"].append(obj["full_name"]),
-            self.result_merged ["actors"].append({"id": obj['person_id'],
+        self.desired_structure ["fw_id"] = obj['film_id']
+        self.desired_structure ['imdb_rating'] = obj['rating']
+        self.desired_structure ['title'] = obj['title']
+        self.desired_structure ["description"] = obj['description']
+        if obj["role"] == "director" and obj["full_name"] != self.desired_structure ["director"]:
+            self.desired_structure ['director'] = obj['full_name']
+        if obj["role"] == "actor" and obj["full_name"] not in self.desired_structure ["actors_names"]:
+            self.desired_structure ["actors_names"].append(obj["full_name"]),
+            self.desired_structure ["actors"].append({"id": obj['person_id'],
                                                 "name": obj["full_name"]})
-        if obj["role"] == "writer" and obj["full_name"] not in self.result_merged ["writers_names"]:
-            self.result_merged ["writers_names"].append(obj["full_name"]),
-            self.result_merged ["writers"].append({"id": obj['person_id'],
+        if obj["role"] == "writer" and obj["full_name"] not in self.desired_structure ["writers_names"]:
+            self.desired_structure ["writers_names"].append(obj["full_name"]),
+            self.desired_structure ["writers"].append({"id": obj['person_id'],
                                                  "name": obj["full_name"]})
 
     def validate_and_return(self):
-        validated_result = FilmWork.parse_obj(self.result_merged)
+        validated_result = FilmWork.parse_obj(self.desired_structure)
         return  validated_result
     def clean(self):
-        self.result_merged = {
+        self.desired_structure = {
             "fw_id": "",
             "imdb_rating": "",
             "genre": [],
