a62b96782af8f8c74c861cb6d33be47651a64d15
Dmitry426 <kachkindmitry@gmail.com>
2022-01-14 19:05:28 +0300

    Code refactor for better readability

--- postgres_to_es/migrate_etl.py
+++ postgres_to_es/migrate_etl.py
@@ -11,25 +11,36 @@ from state_manager import State, JsonFileStorage
 config = Config.parse_file('config.json')
 dsl = dict(config.film_work_pg.dsl)
 sql_query = config.film_work_pg.sql_query
-limit = config.film_work_pg.limit
-file_path = config.film_work_pg.state_file_path
 
+file_path = config.film_work_pg.state_file_path
 json_file_storage = JsonFileStorage(file_path=file_path)
 state = State(json_file_storage)
 updated_at = state.get_state(key='updated_at')
-sql_query_params = f"""
-    ORDER BY updated_at
-    LIMIT {limit};
-"""
-sql_query = sql_query.format(sql_query_params)
+def  handle_no_date():
+    limit = config.film_work_pg.limit
+    if  updated_at is None:
+        sql_query_params = f""" 
+        ORDER BY updated_at
+        LIMIT {limit};
+    """
+    if  updated_at :
+        sql_query_params = f""" 
+        WHERE updated_at > {updated_at}   
+        ORDER BY updated_at
+        LIMIT {limit};
+    """
+    return sql_query_params
+
+sql_query = sql_query.format(handle_no_date())
+
 client = Elasticsearch()
 data_merger = Data_Merger()
 
 with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
     with pg_conn.cursor() as cursor:
         cursor.execute(sql_query)
+        results = []
         for res in iter(lambda: cursor.fetchall(), []):
-            results = []
             film_id_counter = None
             for item in res:
                 fw = dict(item)
@@ -41,7 +52,7 @@ with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
                 if film_id_counter != fw['film_id']:
                     result = data_merger.validate_and_return()
                     results.append(result)
-                    data_merger.clean()
                     film_id_counter = fw['film_id']
                     data_merger.combine_tables(obj=fw)
-        print(len(results))
+                    time = data_merger.last_updated(updated_at=fw['updated_at'])
+                    state.set_state(time.dict())
