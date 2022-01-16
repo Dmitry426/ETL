3e0dc91e3b358c2c81550ebb5f70e70218766ed9
Dmitry426 <kachkindmitry@gmail.com>
2022-01-16 20:49:06 +0300

    ETL migration working / exeptions added

--- postgres_to_es/etl_uploader.py
+++ postgres_to_es/etl_uploader.py
@@ -1,5 +1,8 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch , ElasticsearchException
+from elasticsearch.helpers import  bulk
 import  json
+import logging
+
 class  Upload_batch:
     def __init__(self):
         self.es = Elasticsearch("http://localhost:9200")
@@ -10,12 +13,23 @@ class  Upload_batch:
             self.request_body = json.load(json_file)
 
     def push_index(self):
-        existanse = self.es.search(
-            index="movies",
-            request_timeout=5 )
-        print(existanse)
-        self.create_index()
-        self.es.indices.create(index='movies',body=self.request_body)
+        if not self.es.indices.exists(index="movies"):
+            try:
+                self.create_index()
+                self.es.indices.create(index='movies', body=self.request_body)
+            except ElasticsearchException as es1:
+                logging.error(es1)
+
+    def generate_data(self,data:list):
+        for item in data:
+            yield {
+                "_index": 'movies',
+                "_id": item['id'],
+                '_source':item
+            }
 
     def es_push_butch(self,data:list):
-        pass
+        try:
+            bulk(self.es , self.generate_data(data=data))
+        except ElasticsearchException as es2:
+            logging.error(es2)
