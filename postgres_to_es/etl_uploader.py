1654cc2492094b31331781d913c80068104dfd73
Dmitry426 <kachkindmitry@gmail.com>
2022-01-17 22:33:46 +0300

    New logic for postgres loader , added safe load state

--- postgres_to_es/etl_uploader.py
+++ postgres_to_es/etl_uploader.py
@@ -8,28 +8,30 @@ class  Upload_batch:
         self.es = Elasticsearch("http://localhost:9200")
         self.request_body = None
     
-    def create_index(self):
+    def _create_index(self):
         with open('etl_index.json') as json_file:
             self.request_body = json.load(json_file)
 
-    def push_index(self):
+    def _push_index(self):
+        """Method to keep index automatically updated """
         if not self.es.indices.exists(index="movies"):
             try:
-                self.create_index()
+                self._create_index()
                 self.es.indices.create(index='movies', body=self.request_body)
             except ElasticsearchException as es1:
                 logging.error(es1)
 
-    def generate_data(self,data:list):
+    def _generate_data(self,data:list):
         for item in data:
             yield {
                 "_index": 'movies',
                 "_id": item['id'],
                 '_source':item
             }
-
     def es_push_butch(self,data:list):
+        self._push_index()
         try:
-            bulk(self.es , self.generate_data(data=data))
+            bulk(self.es , self._generate_data(data=data))
+            self.es.transport.close()
         except ElasticsearchException as es2:
             logging.error(es2)
