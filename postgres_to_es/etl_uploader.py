718066fa9908df7c3ae74f996dca09a0cb8b5fee
Dmitry426 <kachkindmitry@gmail.com>
2022-01-18 22:40:05 +0300

    Project wide logger to a file , close connection each after upload

--- postgres_to_es/etl_uploader.py
+++ postgres_to_es/etl_uploader.py
@@ -2,15 +2,17 @@ from elasticsearch import Elasticsearch , ElasticsearchException
 from elasticsearch.helpers import  bulk
 import  json
 import logging
-
+import backoff
 class  Upload_batch:
     def __init__(self):
         self.es = Elasticsearch("http://localhost:9200")
+        self.logger = logging.getLogger('migrate_etl')
         self.request_body = None
     
     def _create_index(self):
         with open('etl_index.json') as json_file:
             self.request_body = json.load(json_file)
+
 
     def _push_index(self):
         """Method to keep index automatically updated """
@@ -19,7 +21,7 @@ class  Upload_batch:
                 self._create_index()
                 self.es.indices.create(index='movies', body=self.request_body)
             except ElasticsearchException as es1:
-                logging.error(es1)
+               self.logger.error(es1)
 
     def _generate_data(self,data:list):
         for item in data:
@@ -34,4 +36,4 @@ class  Upload_batch:
             bulk(self.es , self._generate_data(data=data))
             self.es.transport.close()
         except ElasticsearchException as es2:
-            logging.error(es2)
+            self.logger.error(es2)
