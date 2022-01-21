57147e5275ec04760f92a17eb4b8e8b81cef9bb9
Dmitry426 <kachkindmitry@gmail.com>
2022-01-21 17:40:24 +0300

    .Env autovalidation

--- postgres_to_es/validation_classes.py
+++ postgres_to_es/validation_classes.py
@@ -1,42 +1,41 @@
 from typing import Optional, List
-from pydantic import BaseModel, validator
-from pydantic.validators import UUID
 from datetime import datetime
 
+from pydantic import BaseModel, validator
+from pydantic.validators import UUID
+from pydantic import BaseSettings, Field
 
-class DSNSettings(BaseModel):
-    host: str
-    port: int
-    dbname: str
-    password: str
-    user: str
+class DSNSettings(BaseSettings):
+    host: str = Field(..., env='POSTGRES_HOST')
+    port: str = Field(..., env='POSTGRES_PORT')
+    dbname: str = Field(..., env='POSTGRES_DB')
+    password: str = Field(..., env='POSTGRES_PASSWORD')
+    user: str = Field(..., env="POSTGRES_USER")
 
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
 
 class PostgresSettings(BaseModel):
-    dsl: DSNSettings
     limit: Optional[int]
     film_work_state_field: str
-    genres_state_field:str
-    persons_state_field:str
-    fetch_delay: Optional[float]
+    genres_state_field: str
+    persons_state_field: str
     state_file_path: Optional[str]
     sql_query_film_work_by_id: str
     sql_query_persons: str
     sql_query_genres: str
     sql_query_person_film_work: str
     sql_query_genre_film_work: str
-    sql_query_film_work_by_updated_at:str
+    sql_query_film_work_by_updated_at: str
     elastic_port: str
-
 
 class Config(BaseModel):
     film_work_pg: PostgresSettings
 
-
 class Person(BaseModel):
     id: UUID
     name: str
-
 
 class Datetime_serialization(BaseModel):
     persons_updated_at: Optional[datetime] = None
