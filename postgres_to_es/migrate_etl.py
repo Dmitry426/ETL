4161f6bc7d73878804614130bd51970f4eea6266
Dmitry426 <kachkindmitry@gmail.com>
2022-01-21 17:45:17 +0300

    Minor style fixes and Backoff added

--- postgres_to_es/migrate_etl.py
+++ postgres_to_es/migrate_etl.py
@@ -1,14 +1,19 @@
 import logging
 
+import backoff
 import psycopg2
 from psycopg2.extras import DictCursor
+from dotenv import load_dotenv
+from psycopg2 import OperationalError
 
-from validation_classes import Config
+from validation_classes import Config, DSNSettings
 from film_work_process import Film_work_etl_process
 from persons_process import Persons_etl_process
-from genres_process import  Genres_etl_process
+from genres_process import Genres_etl_process
+
 config = Config.parse_file('config.json')
-dsl = dict(config.film_work_pg.dsl)
+load_dotenv()
+dsl = DSNSettings().dict()
 
 
 def load_loger():
@@ -16,18 +21,23 @@ def load_loger():
                         format='%(name)s - %(levelname)s - %(message)s')
 
 
-def migrate_to_etl(connection):
-    film_work_to_es = Film_work_etl_process(config=config, postgres_connection=connection)
-    film_work_to_es.migrate_film_work()
-    film_work_to_es_by_person = Persons_etl_process(config=config, postgres_connection=connection)
-    film_work_to_es_by_person.migrate_film_work()
-    film_work_to_es_by_genre = Genres_etl_process(config=config, postgres_connection=connection)
-    film_work_to_es_by_genre.migrate_film_work()
+@backoff.on_exception(backoff.expo, OperationalError, max_time=60)
+def migrate_to_etl():
+    with psycopg2.connect(**dsl, cursor_factory=DictCursor) as connection:
+        film_work_to_es = Film_work_etl_process(
+            config=config, postgres_connection=connection)
+        film_work_to_es.migrate_film_work()
+        film_work_to_es_by_person = Persons_etl_process(
+            config=config, postgres_connection=connection)
+        film_work_to_es_by_person.migrate_film_work()
+        film_work_to_es_by_genre = Genres_etl_process(
+            config=config, postgres_connection=connection)
+        film_work_to_es_by_genre.migrate_film_work()
+
 
 if __name__ == '__main__':
     load_loger()
     try:
-        with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
-            migrate_to_etl(connection=pg_conn)
+        migrate_to_etl()
     except psycopg2.OperationalError as e:
         logging.error(e)
