c5d5378cc47ee061c2ffab18a9140f28ea7d80de
Dmitrii <kachkindmitry@gmail.com>
2022-06-17 18:03:30 +0300

    feature: pydantic env config

---
+++ postgres_to_es/etl/config_validation/config.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+
+from pydantic.main import BaseModel
+
+
+class ProducerData(BaseModel):
+    query: str
+    table: str
+    state_field: str
+
+
+class PostgresSettings(BaseModel):
+    state_file_path: Optional[str]
+    limit: Optional[int]
+    order_field: str
+    state_field: str
+
+    producer_queries: List[ProducerData]
+    enricher_query: str
+
+
+class UnifiedSettings(BaseModel):
+    state_file_path: Optional[str]
+    limit: Optional[int]
+    order_field: str
+    state_field: str
+
+    producer_queries: List[ProducerData]
+    enricher_query: str
+
+
+class Config(BaseModel):
+    film_work_pg: PostgresSettings
+    person_pg: UnifiedSettings
+    genre_pg: UnifiedSettings
